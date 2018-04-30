9e32261535942bbab3dbee04992111c522f3ab8e
Josef Benninghoff <jsbenning4@gmail.com>
2018-04-30 22:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let currentUser = "Michael";
 const greeting = "Welcome to Flatbook, "
 const welcomeMessage = greeting + currentUser + "!";
 const excitedWelcomeMessage = greeting.toUpperCase() + currentUser.toUpperCase() + "!";
+const 

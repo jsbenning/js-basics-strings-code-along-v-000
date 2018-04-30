06bea738443d69c1f1477470434a6f6c90e076ad
Josef Benninghoff <jsbenning4@gmail.com>
2018-04-30 22:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = "Michael";
-cont greeting = "Welcome to Flatbook, "
+const greeting = "Welcome to Flatbook, "
 const welcomeMessage = greeting + currentUser + "!";
 const excitedWelcomeMessage = greeting.toUpperCase + currentUser.toUpperCase() + "!";
